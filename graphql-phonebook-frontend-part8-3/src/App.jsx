12b4ad486fc9f1d711c5c8342fd992468e3709aa
Mickel Arroz <arrozmickel@gmail.com>
2026-05-21 21:41:42 -0400

    8B Consultas y variables con nombre

--- graphql-phonebook-frontend-part8-3/src/App.jsx
+++ graphql-phonebook-frontend-part8-3/src/App.jsx
@@ -1,24 +1,65 @@
-import React from 'react'
-import { gql } from '@apollo/client'
-import { useQuery } from '@apollo/client/react'
+import { gql } from "@apollo/client"
+import { useQuery, useLazyQuery } from "@apollo/client/react"
+import { useEffect, useState } from "react"
 
 const ALL_PERSONS = gql`
-query {
-  allPersons  {
-    name
-    phone
-    id
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
   }
-}
+`
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone 
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
 `
 
 const Persons = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON) 
+  const [person, setPerson] = useState(null)
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } })
+  }
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson)
+    }
+  }, [result.data])
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>{person.address.street} {person.address.city}</div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h2>Persons</h2>
       {persons.map(p =>
         <div key={p.name}>
           {p.name} {p.phone}
+          <button onClick={() => showPerson(p.name)} >
+            show address
+          </button> 
         </div>  
       )}
     </div>
@@ -28,12 +69,16 @@ const Persons = ({ persons }) => {
 const App = () => {
   const result = useQuery(ALL_PERSONS)
 
-  if (result.loading)  {
+  if (result.loading) {
     return <div>loading...</div>
   }
 
+  if (result.error) {
+    return <div style={{color: 'red'}}>{result.error.message}</div>
+  }
+
   return (
-    <Persons persons = {result.data.allPersons}/>
+    <Persons persons={result.data?.allPersons || []} />
   )
 }
 
