5b69fdcee2ed6cb2aec9a7d6295e88feacddba39
Mickel Arroz <arrozmickel@gmail.com>
2026-05-21 21:01:04 -0400

    8B Realización de consultas

--- graphql-phonebook-frontend-part8-3/src/App.jsx
+++ graphql-phonebook-frontend-part8-3/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ALL_PERSONS = gql`
+query {
+  allPersons  {
+    name
+    phone
+    id
+  }
+}
+`
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>  
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Persons persons = {result.data.allPersons}/>
   )
 }
 
